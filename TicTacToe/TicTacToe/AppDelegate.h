cbe75889ad57dd23e3710179979b1c3802af1c6c
Golden Thumb <zhijunsheng@mbp2020.local>
2020-12-24 18:21:05 -0500

    created TicTacToe xcode project using ObjC

---
+++ TicTacToe/TicTacToe/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Golden Thumb on 2020-12-24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TicTacToe/TicTacToe/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  TicTacToe
+//
+//  Created by Golden Thumb on 2020-12-24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
